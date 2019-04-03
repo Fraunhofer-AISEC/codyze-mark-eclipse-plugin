f07d59a21bdcff8297c290a72646ac292789ea9c
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-04-03 02:01:06 +0200

    Upgrade to XText 2.17 which now supports Java 11

--- de.fhg.aisec.mark.parent/de.fhg.aisec.mark.tests/src/de/fhg/aisec/mark/tests/XtextParserTest.java
+++ de.fhg.aisec.mark.parent/de.fhg.aisec.mark.tests/src/de/fhg/aisec/mark/tests/XtextParserTest.java
@@ -2,6 +2,7 @@ package de.fhg.aisec.mark.tests;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 
@@ -18,7 +19,7 @@ public class XtextParserTest {
 	@Test
 	public void test() {
 		XtextParser parser = new XtextParser();
-		EObject result = parser.parse("/home/julian/workspace/2018-11-bsi-secure-crypto-lib-tool/code/mark-crymlin-eclipse-plugin/examples/Test/Rules.mark");
+		EObject result = parser.parse(URI.createFileURI("/home/julian/workspace/2018-11-bsi-secure-crypto-lib-tool/code/mark-crymlin-eclipse-plugin/examples/Test/Rules.mark"));
 		assertNotNull(result);
 		XtextParser.dump(result);
 	}
