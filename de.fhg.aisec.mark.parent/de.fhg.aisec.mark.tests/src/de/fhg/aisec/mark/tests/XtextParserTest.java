85348e732cb39ec8bc3604b5dd1ea249e755f25f
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-04-03 11:49:04 +0200

    fix(tests): Typo in method call of parser.parse()

--- de.fhg.aisec.mark.parent/de.fhg.aisec.mark.tests/src/de/fhg/aisec/mark/tests/XtextParserTest.java
+++ de.fhg.aisec.mark.parent/de.fhg.aisec.mark.tests/src/de/fhg/aisec/mark/tests/XtextParserTest.java
@@ -2,7 +2,8 @@ package de.fhg.aisec.mark.tests;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.eclipse.emf.common.util.URI;
+import java.io.File;
+
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 
@@ -19,7 +20,7 @@ public class XtextParserTest {
 	@Test
 	public void test() {
 		XtextParser parser = new XtextParser();
-		EObject result = parser.parse(URI.createFileURI("/home/julian/workspace/2018-11-bsi-secure-crypto-lib-tool/code/mark-crymlin-eclipse-plugin/examples/Test/Rules.mark"));
+		EObject result = parser.parse(new File("/home/julian/workspace/2018-11-bsi-secure-crypto-lib-tool/code/mark-crymlin-eclipse-plugin/examples/Test/Rules.mark"));
 		assertNotNull(result);
 		XtextParser.dump(result);
 	}
