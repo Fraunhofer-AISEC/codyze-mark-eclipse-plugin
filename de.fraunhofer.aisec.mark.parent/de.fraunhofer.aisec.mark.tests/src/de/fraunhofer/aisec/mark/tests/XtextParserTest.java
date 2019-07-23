10004af0752c1c6a02c7eb5d4e6e39201b9772a6
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2019-07-23 09:32:28 +0200

    Adding back renamed de.fraunhofer.aisec.*

---
+++ de.fraunhofer.aisec.mark.parent/de.fraunhofer.aisec.mark.tests/src/de/fraunhofer/aisec/mark/tests/XtextParserTest.java
@@ -0,0 +1,34 @@
+package de.fraunhofer.aisec.mark.tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+import de.fraunhofer.aisec.mark.markDsl.MarkModel;
+import de.fraunhofer.aisec.mark.XtextParser;
+
+/**
+ * Unit test for the MARK standalone parser.
+ * 
+ * @author julian
+ *
+ */
+public class XtextParserTest {
+
+	@Test
+	public void test() {
+		XtextParser parser = new XtextParser();
+		parser.addMarkFile(new File("../../examples/Test/Rules.mark"));
+		HashMap<String, MarkModel> result = parser.parse();
+		assertNotNull(result);
+		for (Entry<String, MarkModel> entry : result.entrySet()) {
+			System.out.println(entry.getKey());
+			XtextParser.dump(entry.getValue());
+		}
+	}
+
+}
